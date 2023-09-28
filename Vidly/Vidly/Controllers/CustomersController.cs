40f51110a3fea762fe4555c37f284a88fe29bbe2
Arda AY <arda.ay@outlook.com.tr>
2023-09-29 02:03:38 +0300

    Eager Loading example for Customer's MembershipType's DiscountRate

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
@@ -31,7 +32,7 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customer = _context.Customers.ToList();
+            var customer = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customer);
         }
         
