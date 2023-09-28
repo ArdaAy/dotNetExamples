f25c43167d60852a645ecb134b41f8ad88e2b2f9
Arda AY <arda.ay@outlook.com.tr>
2023-09-29 02:45:26 +0300

    Exercise 3-2
    - Add birthdate to customer

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -17,6 +17,8 @@ namespace Vidly.Models
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
 
+        public DateTime? Birthdate { get; set; }
+
 
     }
 }
