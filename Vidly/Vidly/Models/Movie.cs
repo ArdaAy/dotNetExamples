cd2c9bf5dcfe6cdf277b2c7d4f7c0d353e920fe9
Arda AY <arda.ay@outlook.com.tr>
2023-09-29 16:48:00 +0300

    Exercise 4-1
    - Adding new Movie
    - Editing existing Movie

--- Vidly/Vidly/Models/Movie.cs
+++ Vidly/Vidly/Models/Movie.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public DateTime ReleaseDate { get; set; }
-        public DateTime DateAdded { get; set; }
+
+        public DateTime? DateAdded { get; set; }
+
+        [Required]
         public int NumberInStock { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
         public Genre Genre { get; set; }
 
     }
