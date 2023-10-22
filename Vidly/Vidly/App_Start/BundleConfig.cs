f67e2323f1997cb2d366fe83d6621f62b8f7acf7
Arda AY <arda.ay@outlook.com.tr>
2023-10-23 02:43:04 +0300

    Toastr and client-side validation for Customer Field

--- Vidly/Vidly/App_Start/BundleConfig.cs
+++ Vidly/Vidly/App_Start/BundleConfig.cs
@@ -14,7 +14,9 @@ namespace Vidly
                         "~/Scripts/bootbox.js",
                         "~/Scripts/DataTables/jquery.datatables.js",
                         "~/Scripts/DataTables/datatables.bootstrap.js",
-                        "~/Scripts/typeahead.bundle.js"));
+                        "~/Scripts/typeahead.bundle.js",
+                        "~/Scripts/toastr.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -32,6 +34,7 @@ namespace Vidly
                       "~/Content/bootstrap-lumen.css",
                       "~/content/datatables/css/datatables.bootstrap.css",
                       "~/content/typeahead.css",
+                      "~/content/toastr.css",
                       "~/Content/site.css"));
         }
     }
